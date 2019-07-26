309f90991e11d8fa7cfcf9b582d95a0dcd10db83
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-07-26 12:09:43 +0200

    2019.07.26 12:09

--- src/Korowai/Component/Ldif/Preprocessor/Preprocessor.php
+++ src/Korowai/Component/Ldif/Preprocessor/Preprocessor.php
@@ -12,27 +12,104 @@ declare(strict_types=1);
 namespace Korowai\Component\Ldif\Preprocessor;
 
 /**
- * An abstract base class for a piece of LDIF file.
+ * Functions that support LDIF text preprocessing.
  */
 trait Preprocessor
 {
-    public static function preprocess(string $src, array &$offsets=null)
+    /**
+     *
+     */
+    public static function preMkJumps(array $pieces) : array
     {
-        $re_comment = '^#(?:[^\r\n]|(?:(?:\r\n|\n) ))*';
-        $re_lncont = '(?:\r\n|\n) ';
-
-        $re = '/((?:' . $re_comment . ')|(?:'. $re_lncont . '))/mu';
-        $pieces = preg_split($re, $src, -1, PREG_SPLIT_OFFSET_CAPTURE | PREG_SPLIT_NO_EMPTY);
-
-        var_dump($pieces);
-
-        $offsets = [];
+        $jumps = [];
         $offset = 0;
         foreach($pieces as $piece) {
-            $offsets[] = [$offset, $piece[1]];
+            $jumps[] = [$offset, $piece[1]];
             $offset += strlen($piece[0]);
         }
-        return implode(array_map(function ($p) {return $p[0];}, $pieces));
+        return $jumps;
+    }
+
+    public static function preCascadeJumps(array $old, array $new) : array
+    {
+        $jumps = [];
+        $ls = 0; // left shrink
+        $ts = 0; // total shrink
+        for($i=0, $j=0; $i < count($old) || $j < count($new); ) {
+            if( $j < count($new)
+                && (    $i >= count($old)
+                    || ($new[$j][0] < $old[$i][0] - $ls &&
+                        $new[$j][1] < $old[$i][0] - $ls)
+                   )
+            ) {
+                // $new[$j] is on the left side of $old[$i]
+                $ls + =($new[$j][1] - $new[$j][0]);
+                $ts + =($new[$j][1] - $new[$j][0]);
+                $jumps[] = [$new[$j][0], $new[$j][0] + $ts];
+                $j++;
+            } elseif($j < count($new) && $i < count($old) &&
+                     $new[$j][0] <= $old[$i][0] - $ls &&
+                     $new[$j][1] >= $old[$i][0] - $ls) {
+                // $new[$j] encloses $old[$i]
+                $ls += ($new[$j][1] - $new[$j][0]);
+                $ts += (($new[$j][1] - $new[$j][0]) + ($old[$i][1] - $old[$i][0]));
+                $jumps[] = [$new[$j][0], $new[$j][0] + $ts];
+                $i++;
+                $j++;
+            } elseif($i < count($old)) {
+                $ts += ($old[$i][1] - $old[$i][0]);
+                $jumps[] = [$old[$i][0] - $ls, $old[$i][0] - $ls + $ts];
+                $i++;
+            }
+        }
+        return $jumps;
+    }
+
+    /**
+     * Updates the array of jumps.
+     *
+     * @param array $pieces
+     * @param array $jumps
+     */
+    public static function preAsmPieces(array $pieces, array &$jumps=null) : string
+    {
+        if($jumps === null) {
+            $jumps = self::preMkJumps($pieces);
+        } else {
+            $jumps = self::preCascadeJumps($jumps, self::preMkJumps($pieces));
+        }
+        return implode(array_map(function ($p){return $p[0];}, $pieces));
+    }
+
+    public static function preCutRe(string $re, string $src, array &$jumps=null) : string
+    {
+        $flags = PREG_SPLIT_OFFSET_CAPTURE | PREG_SPLIT_NO_EMPTY;
+        return self::preAsmPieces(preg_split($re, $src, -1, $flags), $jumps);
+    }
+
+    /**
+     * Removes line continuations from LDIF text (unfolds the lines).
+     *
+     * @param string $src
+     * @param array $jumps
+     * @return string
+     */
+    public static function preCutLnCont(string $src, array &$jumps=null) : string
+    {
+        return self::preCutRe('/(?:\r\n|\n) /mu', $src, $jumps);
+
+    }
+
+    /**
+     * Removes comment lines from LDIF text.
+     *
+     * @param string $src
+     * @param array $jumps
+     * @return string
+     */
+    public static function preCutComments(string $src, array &$jumps=null) : string
+    {
+        return self::preCutRe('/^#(?:[^\r\n]|(?:(?:\r\n|\n) ))*/mu', $src, $jumps);
     }
 }
 

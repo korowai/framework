eecc6a25cb865540658fdaa1b8bfb14c255209df
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-09-06 13:32:19 +0200

    2019.09.06 13:32

--- src/Korowai/Component/Ldif/Util/IndexMap.php
+++ src/Korowai/Component/Ldif/Util/IndexMap.php
@@ -43,51 +43,14 @@ function imFromPieces(array $pieces) : array
  * This shall be used to implement consecutive string manipulations, where
  * each step produces index map array.
  *
- * @param array $old original jump array from previous steps
- * @param array $new a new jump array to be applied to $old
+ * @param array $old original index map array from previous steps
+ * @param array $new a new index map array to be applied to $old
  *
  * @return array
  */
 function imOverIm(array $old, array $new) : array
 {
-    $im = [];
-    $ns = 0; // new shrink (introduced by $new)
-    $ts = 0; // total shrink (cumulation of $old and $new)
-    for($i=0, $j=0; $i < count($old) || $j < count($new); ) {
-        if( $j < count($new) &&
-           ($i >= count($old) || $new[$j][1] < ($old[$i][0] - $ns))) {
-            //
-            // $new[$j] on the left side of $old[$i]
-            //
-            $ts += ($new[$j][1] - $new[$j][0]);
-            $im[] = [$new[$j][0], $new[$j][0] + $ts];
-            $ns += ($new[$j][1] - $new[$j][0]);
-            $j++;
-        } elseif($j < count($new) && $i < count($old) &&
-                 $new[$j][0] <= $old[$i][0] - $ns) {
-            //
-            // $new[$j] encloses $old[$i] (and perhaps $old[$i+1], ...)
-            //
-            $ts += ($new[$j][1] - $new[$j][0]);
-            do {
-                $ts += ($old[$i][1] - $old[$i][0]);
-                $i++;
-            } while($i < count($old) && $new[$j][1] >= $old[$i][0] - $ns);
-            $im[] = [$new[$j][0], $new[$j][0] + $ts];
-            $ns += ($new[$j][1] - $new[$j][0]);
-            $j++;
-        } elseif($i < count($old)) {
-            //
-            // $new[$j] on the right side of $old[$i]
-            //
-            $im[] = [$old[$i][0] - $ns, $old[$i][1]];
-            $ts += ($old[$i][1] - $old[$i][0]);
-            $i++;
-        } else {
-            throw \RuntimeException("internal error");
-        }
-    }
-    return $im;
+    return (new ImOverImAlgorithm($old, $new))();
 }
 
 /**

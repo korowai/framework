d6f0c086d6579c4845d670f7b199b462e2c3c4b7
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-09-17 00:31:14 +0200

    2019.09.17 00:31

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -20,8 +20,6 @@ import os
 from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
 
-os.system('php --version')
-
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     rtd_project = os.environ.get('READTHEDOCS_PROJECT')

b12bb3db254fb96ff0fba387112d8f41ae9578d8
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-09-21 01:22:23 +0200

    ldap component and new packages
    Elaborated a lot on korowai-ldap
    Implemented korowai-contextlib
    Implemented korowai-errorlib
    Started korowai-basiclib

--- docs/sami/sami.conf.php
+++ docs/sami/sami.conf.php
@@ -10,11 +10,11 @@ $iterator = Finder::create()
   ->exclude("Resources")
   ->exclude("Behat")
   ->exclude("vendor")
-  ->in(__DIR__ . "/src");
+  ->in(__DIR__ . "/../../src");
 
-return new Sami( $iterator, array(
+return new Sami($iterator, array(
   'theme'     => 'default',
-  'title'     => 'Korowai - API documentation',
-  'build_dir' => __DIR__ . '/build/docs/api/build/local',
-  'cache_dir' => __DIR__ . '/build/docs/api/cache/local',
+  'title'     => 'Korowai Framework API',
+  'build_dir' => __DIR__ . '/../build/html/api',
+  'cache_dir' => __DIR__ . '/../cache/html/api'
 ));

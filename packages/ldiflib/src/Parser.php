f1e0a4784b0cb835089aaaaca2a7e9df68fd5461
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-02-20 16:36:33 +0100

    2020.02.20 16:36

--- packages/ldiflib/src/Parser.php
+++ packages/ldiflib/src/Parser.php
@@ -13,24 +13,11 @@ declare(strict_types=1);
 
 namespace Korowai\Lib\Ldif;
 
-use Korowai\Lib\Ldif\Traits\ParsesAttrValSpec;
-use Korowai\Lib\Ldif\Traits\ParsesDnSpec;
-use Korowai\Lib\Ldif\Traits\ParsesVersionSpec;
-use Korowai\Lib\Ldif\Traits\ParsesValueSpec;
-
 /**
  * LDIF parser.
  */
 class Parser implements ParserInterface
 {
-    // Include traits in appropriate order. If trait A declares an abstract
-    // method which is documented in B, then A should go first, and B later.
-    // This shall help sami with generating documentation correctly.
-    use ParsesAttrValSpec;
-    use ParsesDnSpec;
-    use ParsesVersionSpec;
-    use ParsesValueSpec;
-
     /**
      * {@inheritdoc}
      */

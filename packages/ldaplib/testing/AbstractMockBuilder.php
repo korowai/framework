4cdba56c5f799aced0d2c232d7f0a7412a016f3b
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2021-10-27 13:42:32 +0200

    2021.10.27 13:42

--- packages/ldaplib/testing/AbstractMockBuilder.php
+++ packages/ldaplib/testing/AbstractMockBuilder.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Korowai\Testing\Ldaplib;
 
 use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\MockObject\MockBuilder;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,86 +22,111 @@ use PHPUnit\Framework\TestCase;
 abstract class AbstractMockBuilder
 {
     private TestCase $testCase;
-    private array $options;
-//    private array $onlyMethods;
+    private MockBuilder $builder;
 
-    public function __construct(TestCase $testCase, array $options = [])
+    public function __construct(TestCase $testCase)
     {
         $this->testCase = $testCase;
-        $this->options = $options;
-//        $this->onlyMethods = appendDefaultMethods($options['onlyMethods'] ?? []);
+        $this->builder = $testCase->getMockBuilder($this->mockedType());
     }
 
-    final public function hasOption(string $key): bool
+    final public function getTestCase(): TestCase
     {
-        return array_key_exists($key, $this->options);
+        return $this->testCase;
     }
 
-    final public function getOption(string $key)
+    final public function getBuilder(): MockBuilder
     {
-        return $this->options[$key] ?? null;
+        return $this->builder;
     }
 
     /**
-     * Returns an array that maps option names onto method names that shall
-     * become available once the given options are provided to constructor.
+     * Returns the name of the mocked type.
+     *
+     * @return string
      */
-    protected function defaultOptionsMethods(): array
-    {
-        return [];
-    }
+    abstract public function mockedType(): string;
 
-    final private function appendDefaultMethods(array $methods = []): array
-    {
-        $optionMethods = $this->defaultOptionsMethods();
-        foreach ($optionMethods as $option => $method) {
-            if ($this->hasOption($option) && !in_array($method, $methods)) {
-                $methods[] = $method;
-            }
-        }
 
-        return $methods;
-    }
-
-    public function setExpectations(MockObject $mock): void
-    {
-        if ($this->hasOption('resource')) {
-            $mock->expects($this->testCase->any())
-                ->method('getResource')
-                ->willReturn($this->getOption('resource'))
-            ;
-        }
-
-        if ($this->hasOption('supportedResourceTypes')) {
-            $mock->expects($this->testCase->any())
-                 ->method('supportsResourceType')
-                 ->willReturnCallback(function (string $type) {
-                     return in_array($type, $this->getOption('supportedResourceTypes'));
-                 });
-        }
-
-        if ($this->hasOption('isValid')) {
-            $mock->expects($this->testCase->any())
-                ->method('isValid')
-                ->willReturn($this->getOption('isValid'));
-        }
-    }
-
-    public function getMockBuilder(): void
-    {
-        return $this->testCase->getMockBuilder($this->getClassName());
-    }
-
-    public function getMock(array $methods = []): void
-    {
-        $builder = $this->getMockBuilder();
-        $builder->onlyMethods($this->appendDefaultMethods($methods));
-        $mock = $builder->getMockForAbstractClass();
-        $this->setExpectations($mock);
-        return $mock;
-    }
-
-    abstract public function getClassName(): string;
+//    /**
+//     * Creates new mock using builder.
+//     *
+//     * An implementation shall return one of:
+//     *
+//     *  - ``$this->builder->getMock()``,
+//     *  - ``$this->builder->getMockForAbstractClass()``, or
+//     *  - ``$this->builder->getMockForTrait()``.
+//     */
+//    abstract public function createMock(): MockObject;
+//
+//    /**
+//     * Returns the wrapped MockBuilder object.
+//     */
+//    final public function getBuilder(): MockBuilder
+//    {
+//        return $this->builder;
+//    }
+//
+//    final public function getMock(): MockObject
+//    {
+//        $builder = $this->getMockBuilder();
+//        $mock = $this->createMock();
+//        $this->implementMockMethods($mock);
+//        return $mock;
+//    }
+//
+////    final public function hasOption(string $key): bool
+////    {
+////        return array_key_exists($key, $this->options);
+////    }
+////
+////    final public function getOption(string $key)
+////    {
+////        return $this->options[$key] ?? null;
+////    }
+////
+////    /**
+////     * Returns an array that maps option names onto method names that shall
+////     * become available once the given options are provided to constructor.
+////     */
+////    protected function defaultOptionsMethods(): array
+////    {
+////        return [];
+////    }
+////
+////    final private function appendDefaultMethods(array $methods = []): array
+////    {
+////        $optionMethods = $this->defaultOptionsMethods();
+////        foreach ($optionMethods as $option => $method) {
+////            if ($this->hasOption($option) && !in_array($method, $methods)) {
+////                $methods[] = $method;
+////            }
+////        }
+////
+////        return $methods;
+////    }
+////
+//    public function implementMockMethods(MockObject $mock): self
+//    {
+//        return $this;
+//    }
+////
+////    public function getMockBuilder(): void
+////    {
+////        return $this->testCase->getMockBuilder($this->getClassName());
+////    }
+////
+////    public function getMock(array $methods = []): void
+////    {
+////        $builder = $this->getMockBuilder();
+////        $builder->onlyMethods($this->appendDefaultMethods($methods));
+////        $mock = $builder->getMockForAbstractClass();
+////        $this->implementMockMethods($mock);
+////        return $mock;
+////    }
+////
+////    abstract protected function validateOptions(array $options): array;
+////    abstract public function getClassName(): string;
 }
 
 // vim: syntax=php sw=4 ts=4 et:
---
+++ packages/ldaplib/tests/testing/AbstractMockBuilderTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of Korowai framework.
+ *
+ * (c) Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+ *
+ * Distributed under MIT license.
+ */
+
+declare(strict_types=1);
+
+namespace Korowai\Tests\Testing\Ldaplib;
+
+use Korowai\Testing\Ldaplib\AbstractMockBuilder;
+use PHPUnit\Framework\MockObject\MockBuilder;
+
+/**
+ * @author Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+ * @covers \Korowai\Testing\Ldaplib\AbstractMockBuilder
+ *
+ * @internal
+ */
+final class AbstractMockBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    protected function createAbstractMockBuilderMock(string $mockedType = null)
+    {
+        $builder = $this->getMockBuilder(AbstractMockBuilder::class);
+        if ($mockedType !== null) {
+            $mock = $builder->disableOriginalConstructor()
+                            ->getMockForAbstractClass();
+            $mock->expects($this->any())
+                 ->method('mockedType')
+                 ->willReturn($mockedType);
+            $mock->__construct($this);
+        } else {
+            $mock = $builder->setConstructorArgs([$this])
+                            ->getMockForAbstractClass();
+        }
+        return $mock;
+    }
+
+    public function testGetTestCase(): void
+    {
+        $builder = $this->createAbstractMockBuilderMock();
+        $this->assertInstanceOf(self::class, $builder->getTestCase());
+    }
+
+    public function testGetBuilder(): void
+    {
+        $builder = $this->createAbstractMockBuilderMock(\Exception::class);
+
+        $this->assertInstanceOf(MockBuilder::class, $builder->getBuilder());
+        $this->assertInstanceOf(\Exception::class, $builder->getBuilder()->getMock());
+    }
+}
+
+// vim: syntax=php sw=4 ts=4 et:
